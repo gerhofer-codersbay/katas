b0bc178472e209bbc8cc7e900c2dd8cbde5c7a57
codersbay <codersbay@DESKTOP-V5FP3TS>
2019-09-23 11:12:52 +0200

    test

--- Katas/src/at/codersbay/katas/Main.java
+++ Katas/src/at/codersbay/katas/Main.java
@@ -6,10 +6,12 @@ public class Main {
 
         System.out.println("After you finished your Kata, make sure to call it from the main class to show us what you've done!");
 
-        // falls du eine TicTacToe klasse geschrieben hast die mit einer statischen Methode ein Tic Tac Toe feld auf die Konsole drucken kann könnte das so aussehen:
+        // falls du eine TicTacToe klasse geschrieben hast die mit einer statischen Methode ein Tic Tac Toe feld auf die Konsole drucken kann kÃ¶nnte das so aussehen:
         // eg.: TicTacToe.printRandomField();
         // eg.: TicTacToe.printField("X", "", "O", "", "", "", "X", "X", "O");
-
+        
+        
+        
     }
 
 }
