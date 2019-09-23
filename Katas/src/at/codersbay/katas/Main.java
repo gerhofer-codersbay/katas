4196a587ee3ef929a71b921830614e8c5610deb4
Digi-Mindset <gerhofer.pia@gmx.at>
2019-09-23 10:24:25 +0200

    Add main class

---
+++ Katas/src/at/codersbay/katas/Main.java
@@ -0,0 +1,15 @@
+package at.codersbay.katas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("After you finished your Kata, make sure to call it from the main class to show us what you've done!");
+
+        // falls du eine TicTacToe klasse geschrieben hast die mit einer statischen Methode ein Tic Tac Toe feld auf die Konsole drucken kann könnte das so aussehen:
+        // eg.: TicTacToe.printRandomField();
+        // eg.: TicTacToe.printField("X", "", "O", "", "", "", "X", "X", "O");
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # katas
+
+Katas sind kleine Übungen um im Programmieren besser zu werden. 
+Musiker, Sportler und andere Künstler üben ihre Tätigkeit um besser zu werden - das können und sollten auch Programmierer machen ;) 
+Hintergrund info auf wikipedia: https://de.wikipedia.org/wiki/Kata_%28Programmierung%29
+Webseite mit vielen Katas: https://www.codewars.com/dashboard
