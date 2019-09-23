c2583c93e9eab21864acfba08e2bae1b236360a1
M Hofmair <michael@hofmair.net>
2019-09-23 14:12:39 +0200

    :sparkles: Application is able to transform Strings to Leadspeak

---
+++ Katas/src/Leetspeak/Leetspeak.java
@@ -0,0 +1,45 @@
+package Leetspeak;
+
+/**
+ * @package: Leetspeak;
+ * @created: 23.09.2019 by Coderbay
+ * @licence: proprietary
+ */
+public class Leetspeak {
+    public String fromString(String text) {
+        String leetspeak="";
+        for (int i = 0; i < text.length(); i++) {
+            char charAtI=text.charAt(i);
+            switch (charAtI){
+                case 'a':
+                    leetspeak+="4";
+
+                    break;
+                case 'e':
+                    leetspeak+="3";
+
+                    break;
+                case 'l':
+                    leetspeak+="1";
+
+                    break;
+                case 'm':
+                    leetspeak+="/^^\\";
+
+                    break;
+                case 'o':
+                    leetspeak+="0";
+
+                    break;
+                case 'u':
+                    leetspeak+="(_)";
+
+                    break;
+                default:
+                    leetspeak+=text.charAt(i);
+            }
+
+        }
+        return leetspeak;
+    }
+}
---
+++ Katas/src/Main.java
@@ -0,0 +1,23 @@
+import Leetspeak.Leetspeak;
+
+/**
+ * @package: PACKAGE_NAME;
+ * @created: 23.09.2019 by Coderbay
+ * @licence: proprietary
+ */
+public class Main {
+    public static void main(String[] args) {
+        Leetspeak leetspeak = new Leetspeak();
+        System.out.println(leetspeak.fromString(" My text is long and beautiful, as, it has been well documented, " +
+                "are various other parts of my website.Lorem Ispum is a choke artist. It chokes! Some people have an " +
+                "ability to write placeholder text... It's an art you're basically born with. You either have it or " +
+                "you don't. I think the only difference between me and the other placeholder text is that I’m more " +
+                "honest and my words are more beautiful. Lorem Ipsum best not make any more threats to your website. " +
+                "It will be met with fire and fury like the world has never seen.Lorem Ipsum is unattractive, both " +
+                "inside and out. I fully understand why it’s former users left it for something else. They made a good " +
+                "decision. Some people have an ability to write placeholder text... It's an art you're basically born " +
+                "with. You either have it or you don't.I'm speaking with myself, number one, because I have a very good " +
+                "brain and I've said a lot of things. My text is long and beautiful, as, it has been well documented, " +
+                "are various other parts of my website."));
+    }
+}
